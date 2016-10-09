4bf74f8ee3aa7c06918d691e906ae4def4453455
Paul Kramme <pjkramme@gmail.com>
2016-10-09 17:58:15 +0200

    Add setup and config function

--- clitimer.py
+++ clitimer.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
 import time
-import argparse
+import sys
 
-parser = argparse.ArgumentParser("Simple CLI Counter")
-parser.add_argument("time", metavar="T", type=int, nargs=1, help="Time when the alarm will go off")
+silent = None
+time = None
+einheit = None
 
-args = parser.parse_args()
-print(args)
 
+def help():
+    print("Usage: " + sys.argv[0] + "time einheit [args]")
+    print("--silent or -s for silentmode")
+    print("--help or -h for help")
+    print("Happy timing...")
+
+
+def setup():
+    print("setup")
+
+
+def config():
+    if "--silent" in sys.argv:
+        silent = True
+        print("Silent Activated")
+    elif "-s" in sys.argv:
+        silent = True
+    if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        help()
+    else:
+        time = sys.argv[1]
+        einheit = sys.argv[2]
+        print(time, einheit)
+
+
+def main():
+    config()
+if __name__ == __name__:
+    main()
