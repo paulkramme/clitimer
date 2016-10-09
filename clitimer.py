ed36277743ee4529c516e41e36a8f5e9361cfb6f
Paul Kramme <pjkramme@gmail.com>
2016-10-09 11:54:31 +0200

    Add argparse

--- clitimer.py
+++ clitimer.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
 import time
 import sys
+import argparse
 
-cliinput = ""
+parser = argparse.ArgumentParser("Simple CLI Counter")
+parser.add_argument("time", metavar="T", type=int, nargs=1, help="Time when the alarm will go off")
 
-def config():
-	if "--silent", "-s" in cliinput:
-		print("WHEEEEE")
-	elif "--count" in sys.argv:
-		print("Counting something")
-	elif "--count" in cliinput:
-		print("ur mam")
-	else:
-		print("Command not found")
-def main():
-	if len(sys.argv) < 2:
-		print("CLI Counter")
-		while True:
-			print(">", end='')
-			cliinput = input()
-			config()
-	else:
-		config()
-	
-
-if __name__ == __name__:
-	main()
-else:
-	print("This is not a lib")
+args = parser.parse_args()
+print(args)
